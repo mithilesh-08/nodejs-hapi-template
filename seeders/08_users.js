4f0379a61571da831da7788ad706ba76d2e7ee5a
Mithilesh Pandit <mp@Mithileshs-MacBook-Pro.local>
2025-04-21 15:08:08 +0530

    Added DAOs and tests for vehicle, trip, trip locations, payment systems, redis for trip requests as well

--- seeders/08_users.js
+++ seeders/08_users.js
@@ -1,53 +1,57 @@
-
 const { v4: uuidv4 } = require('uuid');
 const bcrypt = require('bcrypt');
 
 module.exports = {
-  up: async(queryInterface) => {
+  up: async (queryInterface) => {
     const roles = await queryInterface.sequelize.query(
       'SELECT id, name FROM roles;',
-      { type: queryInterface.sequelize.QueryTypes.SELECT }
+      { type: queryInterface.sequelize.QueryTypes.SELECT },
     );
     const arr = [
       {
         id: uuidv4(),
         name: 'Mithilesh',
-        role_id: roles.find(role => role.name === 'SUPER_ADMIN').id,
+        role_id: roles.find((role) => role.name === 'SUPER_ADMIN').id,
         password: bcrypt.hashSync('password', 10),
         phone_number: '7350977851',
         email: 'mithilesh@wednesday.is',
         created_at: new Date(),
         updated_at: new Date(),
+        oauth_client_id: 1,
       },
       {
         id: uuidv4(),
         name: 'Raj',
-        role_id: roles.find(role => role.name === 'ADMIN').id,
+        role_id: roles.find((role) => role.name === 'ADMIN').id,
         password: bcrypt.hashSync('password', 10),
         phone_number: '7350977852',
         email: 'raj@wednesday.is',
         created_at: new Date(),
         updated_at: new Date(),
-      },{
+        oauth_client_id: 2,
+      },
+      {
         id: uuidv4(),
-        name:'Rajesh',
-        role_id: roles.find(role => role.name === 'DRIVER').id,
+        name: 'Rajesh',
+        role_id: roles.find((role) => role.name === 'DRIVER').id,
         password: bcrypt.hashSync('password', 10),
         phone_number: '7350977853',
         email: 'rajesh@gmail.com',
         created_at: new Date(),
         updated_at: new Date(),
+        oauth_client_id: 3,
       },
       {
         id: uuidv4(),
-        name:'Ramesh',
-        role_id: roles.find(role => role.name === 'RIDER').id,
+        name: 'Ramesh',
+        role_id: roles.find((role) => role.name === 'RIDER').id,
         password: bcrypt.hashSync('password', 10),
         phone_number: '7350977854',
         email: 'ramesh@gmail.com',
         created_at: new Date(),
         updated_at: new Date(),
-      }
+        oauth_client_id: 4,
+      },
     ];
     return queryInterface.bulkInsert('users', arr, {});
   },
