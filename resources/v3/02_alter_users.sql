7880b218084745c45ac3acc2103855777a7e7ff9
Mithilesh Pandit <mp@Mithileshs-MacBook-Pro.local>
2025-04-21 17:55:06 +0530

    fix: removed indexs and fixed migrations

--- resources/v3/02_alter_users.sql
+++ resources/v3/02_alter_users.sql
@@ -1,8 +1,6 @@
--- Add foreign key constraint for users table
 ALTER TABLE users
-    ADD CONSTRAINT users_oauth_clients_id_fk 
-    FOREIGN KEY (oauth_client_id)
-    REFERENCES oauth_clients(id) ON UPDATE CASCADE,
-    ADD CONSTRAINT fk_users_role_id 
+    ADD CONSTRAINT fk_users_role_id
     FOREIGN KEY (role_id) 
-    REFERENCES roles(id) ON DELETE SET NULL;
+    REFERENCES roles(id) ON DELETE SET NULL,
+    ADD INDEX idx_users_phone_number (phone_number);
+
